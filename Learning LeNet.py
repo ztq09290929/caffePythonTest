6d35eb8976c5d5ff2c17e5288ac9830b1c6485f9
ztq09290929 <lovesampo@163.com>
2016-05-30 19:24:32 +0800

    添加文件

--- Learning LeNet.py
+++ Learning LeNet.py
@@ -12,3 +12,64 @@ sys.path.append('/home/ztq/GitWorkSpace/caffePythonTest/')
 sys.path.append('/home/ztq/caffe/python/')
 import caffe
 import CaffePythonTools as mytools
+import os
+os.chdir('/home/ztq/caffe')
+
+caffe.set_mode_gpu()
+
+### load the solver and create train and test nets
+solver = None  # ignore this workaround for lmdb data (can't instantiate two solvers on the same data)
+solver = caffe.SGDSolver('examples/mnist/lenet_solver.prototxt')
+
+# each output is (batch size, feature dim, spatial dim)
+print [(k, v.data.shape) for k, v in solver.net.blobs.items()]
+
+# just print the weight sizes (we'll omit the biases)
+print [(k, v[0].data.shape) for k, v in solver.net.params.items()]
+
+#solver.net.forward()  # train net
+#solver.test_nets[0].forward()  # test net (there can be more than one)
+# we use a little trick to tile the first eight images
+#imshow(solver.net.blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray'); axis('off')
+#print 'train labels:', solver.net.blobs['label'].data[:8]
+#imshow(solver.test_nets[0].blobs['data'].data[:8, 0].transpose(1, 0, 2).reshape(28, 8*28), cmap='gray'); axis('off')
+#print 'test labels:', solver.test_nets[0].blobs['label'].data[:8]
+
+niter = 200
+test_interval = 25
+# losses will also be stored in the log
+train_loss = zeros(niter)
+test_acc = zeros(int(np.ceil(niter / test_interval)))
+output = zeros((niter, 8, 10))
+
+# the main solver loop
+for it in range(niter):
+    solver.step(1)  # SGD by Caffe
+    
+    # store the train loss
+    train_loss[it] = solver.net.blobs['loss'].data
+    
+    # store the output on the first test batch
+    # (start the forward pass at conv1 to avoid loading new data)
+    solver.test_nets[0].forward(start='conv1')
+    output[it] = solver.test_nets[0].blobs['ip2'].data[:8]
+    # run a full test every so often
+    # (Caffe can also do this for us and write to a log, but we show here
+    #  how to do it directly in Python, where more complicated things are easier.)
+    if it % test_interval == 0:
+        print 'Iteration', it, 'testing...'
+        correct = 0
+        for test_it in range(100):
+            solver.test_nets[0].forward()
+            correct += sum(solver.test_nets[0].blobs['ip2'].data.argmax(1)
+                           == solver.test_nets[0].blobs['label'].data)
+        test_acc[it // test_interval] = correct / 1e4
+
+_, ax1 = subplots()
+ax2 = ax1.twinx()
+ax1.plot(arange(niter), train_loss)
+ax2.plot(test_interval * arange(len(test_acc)), test_acc, 'r')
+ax1.set_xlabel('iteration')
+ax1.set_ylabel('train loss')
+ax2.set_ylabel('test accuracy')
+ax2.set_title('Test Accuracy: {:.2f}'.format(test_acc[-1]))
---
+++ R-CNN detection.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 30 15:25:59 2016
+
+@author: root
+"""
+
+from pylab import *
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+sys.path.append('/home/ztq/GitWorkSpace/caffePythonTest/')
+sys.path.append('/home/ztq/caffe/python/')
+import caffe
+import CaffePythonTools as mytools
+import os
+os.chdir('/home/ztq/caffe')
+
